19943dade15d8ed8778568faa9ce6186853d2c33
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-02-12 18:56:50 +0600

    commit now

--- IT_SERVICES/settings.py
+++ IT_SERVICES/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-(aryth*dxi9%*#1y$p!&yas0$icapgk_cz(tu1ntd#jv#zo9o=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
