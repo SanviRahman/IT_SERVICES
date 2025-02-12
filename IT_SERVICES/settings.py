448021860891347231d368b3073afe021ebf7981
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-02-12 22:45:54 +0600

    all okay good to go

--- IT_SERVICES/settings.py
+++ IT_SERVICES/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-(aryth*dxi9%*#1y$p!&yas0$icapgk_cz(tu1ntd#jv#zo9o=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.onrender.com','.vercel.app','.now.sh']
 
 
 # Application definition
