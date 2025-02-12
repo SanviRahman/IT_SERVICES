38c6fe53676533b6b1d6efe515721f835fae800f
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-02-12 23:28:28 +0600

    abar hobe INSHALLAH

--- myApp/views.py
+++ myApp/views.py
@@ -1,31 +1,23 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .forms import ContactModelForm
-from .models import Services,SliderOne,SliderTwo
+from .models import Services, SliderOne, SliderTwo
 
 
 # Create your views here.
 def landing(request):
-    if request.method == 'GET':
-        form=SliderOne.objects.all()
-        forms=SliderTwo.objects.all()
-    return render(request, "index.html", {'form': form,'forms':forms})
-
-
+    form = SliderOne.objects.all()  
+    forms = SliderTwo.objects.all()  
+    return render(request, "index.html", {'form': form, 'forms': forms})
 
 
 def service(request):
-    if request.method == 'GET':
-        form=Services.objects.all()
-    return render(request,'service.html',{'form': form})
+    form = Services.objects.all()  
+    return render(request, 'service.html', {'form': form})
 
 
-
-def serviceDetails(request,pk):
-    if request.method == 'GET':
-        form=Services.objects.get(pk=pk)
-    return render(request,'serviceDetails.html',{'form': form})
-
-
+def serviceDetails(request, pk):
+    form = Services.objects.get(pk=pk) 
+    return render(request, 'serviceDetails.html', {'form': form})
 
 
 def contact(request):
@@ -39,14 +31,9 @@ def contact(request):
     return render(request, "contact.html", {"form": form})
 
 
-
 def about(request):
-    return render(request, 'about.html') 
+    return render(request, 'about.html')
 
 
 def moreabout(request):
     return render(request, 'moreabout.html')
-
-
-
-
