db254cdd6a6a212e38bfa7b88df4067a8f2f8d5b
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-02-12 23:00:28 +0600

    last try

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import service,serviceDetails,about,landing,contact,moreabout
 
 urlpatterns = [
-    path('', landing, name='landing'),
+    path('landing/', landing, name='landing'),
     path('contact/',contact,name='contact'),
     path('service/',service,name='service'),
     path('serviceDetails/<int:pk>/',serviceDetails,name='serviceDetails'),
