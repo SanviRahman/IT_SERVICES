9f9d3536354d3fd9db9e1730fd4d678032e3ca4f
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-02-12 22:20:31 +0600

    last try

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
+#!/bin/sh
 echo "BUILD START"
-python3.13.1 -m pip install -r requirments.txt
-python3.13.1 manage.py collectstatic --noinput --clear
+
+# Use a compatible Python version (adjust if necessary)
+python3 -m pip install -r requirements.txt
+
+# Run Django static files collection
+python3 manage.py collectstatic --noinput --clear
+
+# Ensure Vercel finds the output directory
+mkdir -p staticfiles_build
+cp -r staticfiles/* staticfiles_build/
+
 echo "BUILD END"
